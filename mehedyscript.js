7e123154f6739dfc851cda2bdd4c59b9185d71dd
freefireob49v2 <freefireob49v2@gmail.com>
2026-07-08 20:24:20 +0600

    Add files via upload

--- mehedyscript.js
+++ mehedyscript.js
@@ -213,31 +213,7 @@
       } catch { /* silent */ }
     });
 
- 
- 
- 
- 
-    loginBtn.addEventListener("click", async () => {
-      const inputKey = keyInput.value.trim();
-
-      if (!inputKey) {
-        statusEl.innerHTML = "<span style='color:#ff4444;'>PLEASE INPUT KEY!</span>";
-        return;
-      }
-
-      statusEl.innerHTML = "<span style='color:#00ffcc; text-shadow:0 0 8px rgba(0,255,204,0.3);'>CONNECTING SERVER...</span>";
-      loginBtn.disabled = telegramBtn.disabled = true;
-      try {
-        const keyRes  = await fetch(CONFIG.k + "?t=" + Date.now());
-        const keyText = await keyRes.text();
-        const validKeys = keyText
-          .split("\n")
-          .map(k => k.trim())
-          .filter(k => k !== "");
-
-        if (validKeys.includes(inputKey)) {
- 
- 
+  
  
  
           statusEl.innerHTML = "<span style='color:#00ffcc;'>KEY VALIDATED! ✓</span>";
